f049fa1c5988c23d4d3b92ec5c717eb95d2e8d41
lizhe <lizhenhui1122@163.com>
2020-03-17 17:31:52 +0800

    kick off

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <el-container>
+      <el-header style="background-color: #2c3e50">
+        <router-link to="/" tag="el-button" style="float: left; margin-top: 10px">回到主页</router-link>
+      </el-header>
+      <el-main>
+        <router-view />
+      </el-main>
+    </el-container>
   </div>
 </template>
 
